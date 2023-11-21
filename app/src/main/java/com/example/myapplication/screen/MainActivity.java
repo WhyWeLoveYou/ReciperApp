052069e3faa33d2e974474d57bc93039fd69d219
Alipp <alifventura613@gmail.com>
2023-11-21 12:40:00 +0700

    add binding

--- app/src/main/java/com/example/myapplication/screen/MainActivity.java
+++ app/src/main/java/com/example/myapplication/screen/MainActivity.java
@@ -1,17 +1,27 @@
-package com.example.myapplication;
+package com.example.myapplication.screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        db = FirebaseFirestore.getInstance();
+    }
+
+    private void createUser() {
+        firebaseAuth.createUserWithEmailAndPassword("email", "password");
+        db.collection("users");
     }
 }
