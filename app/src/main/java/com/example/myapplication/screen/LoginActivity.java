f8f31c24ecd4dc063be844089fa097f9408d416e
Alipp <alifventura613@gmail.com>
2023-11-21 14:16:44 +0700

    Buat logika sign up

--- app/src/main/java/com/example/myapplication/screen/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/screen/LoginActivity.java
@@ -6,13 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.myapplication.databinding.ActivitySignInBinding;
 import com.example.myapplication.helper.helpp;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
