290b45cc8d0199800ecf4c3857b8fdb672a9405a
Alipp <alifventura613@gmail.com>
2023-11-21 14:15:59 +0700

    Buat logika sign up

--- app/src/main/java/com/example/myapplication/screen/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/screen/LoginActivity.java
@@ -1,12 +1,18 @@
 package com.example.myapplication.screen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.myapplication.databinding.ActivitySignInBinding;
+import com.example.myapplication.helper.helpp;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -14,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private ActivitySignInBinding binding;
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore db;
+    private Context context = getApplicationContext();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +36,31 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void listener() {
-        binding.ButtonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignUp.class);
-                startActivity(intent);
-                finish();
-            }
+        binding.ButtonLogin.setOnClickListener(v -> {
+            validator();
         });
     }
 
     private void userLogin() {
-        firebaseAuth.signInWithEmailAndPassword("email", "password");
+        String email = binding.InputEmal.getText().toString();
+        String password = binding.InputPw.getText().toString();
+        firebaseAuth.signInWithEmailAndPassword("email", "password").addOnCompleteListener(task -> {
+            new helpp(context).showToast("Anda Berhasil Login");
+            Intent intent = new Intent(LoginActivity.this, SignUp.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+    }
+
+    private void validator() {
+        String email = binding.InputEmal.getText().toString();
+        String password = binding.InputPw.getText().toString();
+        if (email.isEmpty()) {
+            new helpp(context).showToast("Silahkan memasukan email");
+        } else if (password.isEmpty()) {
+            new helpp(context).showToast("Silahkan memasukan password");
+        } else {
+            userLogin();
+        }
     }
 }
