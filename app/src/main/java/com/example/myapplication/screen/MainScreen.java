49dc180032ee8d43221dbcd76bd0b03aa2c8eee4
Alipp <alifventura613@gmail.com>
2023-11-22 14:35:35 +0700

    Fix code

--- app/src/main/java/com/example/myapplication/screen/MainScreen.java
+++ app/src/main/java/com/example/myapplication/screen/MainScreen.java
@@ -1,21 +1,30 @@
 package com.example.myapplication.screen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.ActivityMainScreenBinding;
+import com.example.myapplication.databinding.ProfileBinding;
 import com.example.myapplication.screen.fragment.HomeFragment;
 import com.example.myapplication.screen.fragment.ProfileFragment;
 import com.example.myapplication.screen.fragment.ReceiptFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainScreen extends AppCompatActivity {
 
-    ActivityMainScreenBinding binding;
+    private ActivityMainScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,34 +33,37 @@ public class MainScreen extends AppCompatActivity {
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
-        binding.bottomAppBar.setOnMenuItemClickListener( item -> {
-
-            switch (item.getItemId()){
-
-                case R.id.home:
-                    replaceFragment(new HomeFragment());
-                    break;
-                case R.id.receipt:
-                    replaceFragment(new ReceiptFragment());
-                    break;
-                case R.id.profile:
-                    replaceFragment(new ProfileFragment());
-                    break;
-                default:
-                    break;
-
+        binding.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        binding.BnewChat.setVisibility(View.VISIBLE);
+                        replaceFragment(new HomeFragment());
+                        break;
+                    case R.id.receipt:
+                        binding.BnewChat.setVisibility(View.GONE);
+                        replaceFragment(new ReceiptFragment());
+                        break;
+                    case R.id.profile:
+                        replaceFragment(new ProfileFragment());
+                        binding.BnewChat.setVisibility(View.GONE);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
             }
-
-            return true;
         });
-
     }
 
-    private void replaceFragment(Fragment fragment){
+
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
     }
+
 
 }
