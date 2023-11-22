fb7bc5286c968f457b1212d6122a16c34f2f65c1
Kayo <kayome7@gmail.com>
2023-11-22 09:41:09 +0700

    baru

--- app/src/main/java/com/example/myapplication/screen/fragment/ReceiptFragment.java
+++ app/src/main/java/com/example/myapplication/screen/fragment/ReceiptFragment.java
@@ -7,16 +7,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 
 
 public class ReceiptFragment extends Fragment {
 
+    ImageView imageView;
+
+    @Override
+    public void  onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receipt, container, false);
+        View view = inflater.inflate(R.layout.fragment_receipt,container,false);
+
+        imageView = (ImageView) imageView.findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Toast.makeText(getActivity(),"Tunggu Sebentar",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
