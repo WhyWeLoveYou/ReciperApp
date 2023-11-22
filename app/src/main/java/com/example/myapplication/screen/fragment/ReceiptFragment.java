0691559c16086bac80e423a7cd44d6f56dff4473
Nomu1Nomu1 <isaiah.tino@gmail.com>
2023-11-22 09:03:37 +0700

    aowkoakoawaokw file folder adapter

--- app/src/main/java/com/example/myapplication/screen/fragment/ReceiptFragment.java
+++ app/src/main/java/com/example/myapplication/screen/fragment/ReceiptFragment.java
@@ -2,6 +2,7 @@ package com.example.myapplication.screen.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,10 +14,43 @@ import com.example.myapplication.R;
 
 public class ReceiptFragment extends Fragment {
 
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public ReceiptFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ReceiptFragment newInstance(String param1, String param2) {
+        ReceiptFragment fragment = new ReceiptFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1); mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_receipt, container, false);
     }
+
+    @Override
+
+
 }
