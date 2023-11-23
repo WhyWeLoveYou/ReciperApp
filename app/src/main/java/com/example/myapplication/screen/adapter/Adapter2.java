7522c4d5514716fb16b0755ad0774868199f75a3
SkipperJava <144305227+SkipperJava@users.noreply.github.com>
2023-11-23 07:17:40 +0700

    adapter 2 otw bikin

---
+++ app/src/main/java/com/example/myapplication/screen/adapter/Adapter2.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.screen.adapter;
+
+import android.os.Bundle;
+
+public class Adapter2  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate();
+    }
+
+}
